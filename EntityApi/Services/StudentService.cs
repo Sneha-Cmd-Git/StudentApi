628ee331a7ce7b5be0404dc95aadc067ad16cb6a
sneha-fdh <sneha.pal@ifdhi.co.in>
2026-04-07 13:53:34 +0530

    changes done

--- EntityApi/Services/StudentService.cs
+++ EntityApi/Services/StudentService.cs
@@ -15,10 +15,23 @@ namespace EntityApi.Services
         public async Task<IEnumerable<Student>> GetAllStudents() =>
             await _repo.GetAll();
 
+        public async Task<Student> GetStudentById(int id) =>
+            await _repo.GetById(id);
+
         public async Task AddStudent(Student student)
         {
             student.CreatedDate = DateTime.Now;
             await _repo.Add(student);
+        }
+
+        public async Task UpdateStudent(Student student)
+        {
+            await _repo.Update(student);
+        }
+
+        public async Task DeleteStudent(int id)
+        {
+            await _repo.Delete(id);
         }
     }
 }
